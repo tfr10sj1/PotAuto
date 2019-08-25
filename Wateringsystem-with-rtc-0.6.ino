7ffcf288c1383aedaba3a0c8571cf54f5e150ddd
tfr10sj1 <32736648+tfr10sj1@users.noreply.github.com>
2019-08-25 12:29:11 +0200

    new update

--- Wateringsystem-with-rtc-0.6.ino
+++ Wateringsystem-with-rtc-0.6.ino
@@ -83,7 +83,7 @@ void setup() {
    // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
     // This line sets the RTC with an explicit date & time, for example to set
     // January 21, 2014 at 3am you would call:
-    rtc.adjust(DateTime(2019, 8, 11, 15, 51, 0));
+    rtc.adjust(DateTime(2019, 8, 25, 12, 28, 0));
   }
   
   WiFi.begin(ssid, password);
@@ -704,8 +704,8 @@ void ShowHistory()
       History[i] = " ";
     }
   }
-  History[oldDay.toInt()] = History[oldDay.toInt()]+"\n"+oldMonth+"-"+oldDay+"-"+oldHour +":"+ newMinute + "\n";
-  //handleserver();
+  History[oldDay.toInt()] = History[oldDay.toInt()]+"\n"+oldYear+"/"+oldMonth+"/"+oldDay+"-"+oldHour +":"+ newMinute + "\n";
+  handleserver();
 }
 void dirandtime(int vinkel)
 {
